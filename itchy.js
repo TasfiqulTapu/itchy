e2f2f7aa85d3b298d5a967e1adde79ab6681cdcb
Glitch (glitch-blank-node) <none>
2022-06-06 08:09:56 +0000

    🚐🛋 Checkpoint ./itchy.js:18182267/542 ./server.js:18182267/6

--- itchy.js
+++ itchy.js
@@ -4,10 +4,14 @@ const cheerio = require("cheerio")
 module.exports = {getJamData};
 
 async function getJamData(jamURL){
-  let x = await getItchData(jamURL)
+  let jam = {}
+  let x = await getItchData(jamURL);
   const $ = cheerio.load(x.data);
-  let div = $('div[class="stats_container"]')['0'];
-  console.log(div)
+  //let div = $('div[class="stats_container"]')['0'].children.forEach(e=>console.log(e));
+  jam["title"] = $('h1[class="jam_title_header"]').text()
+  let y =  $('div[class="stat_box"]').html()
+  console.log(y)
+  console.log(jam)
 } 
 
 async function getItchData(pageURL){
