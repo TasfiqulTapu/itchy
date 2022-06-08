7107554420c57d53866fee9c4308b8b27bff3e5d
Glitch (scrawny-heavy-structure) <none>
2022-06-08 16:55:56 +0000

    🐘 Fixed undefined value for past jam entries

--- test.js
+++ test.js
@@ -3,5 +3,7 @@ const itchy = require("./src/itchy")
 async function jam(){
   let jam = await itchy.getJamData("https://itch.io/jam/adventure-jam-2022");
   console.log(jam)
+  jam = await itchy.getJamData("https://itch.io/jam/gmtk-2021");
+  console.log(jam)
 }
 jam()
